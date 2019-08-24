99d73b429f3eadda7b3c119de387aaac5518f0b7
Miguel Silva <mnsilva2@gmail.com>
2019-08-24 13:21:07 +0000

    Removed source maps

--- gulpfile.js
+++ gulpfile.js
@@ -18,8 +18,9 @@ var size = require('gulp-size');
 var uglify = require('gulp-uglify');
 var zip = require('gulp-zip');
 var source = require('vinyl-source-stream');
-
-program.on('--help', function(){
+var sourcemaps = require('gulp-sourcemaps');
+var ignore = require('gulp-ignore')
+program.on('--help', function () {
   console.log('  Tasks:');
   console.log();
   console.log('    build       build the game');
@@ -38,10 +39,27 @@ program
 var prod = !!program.prod;
 
 gulp.task('default', ['build']);
-gulp.task('build', ['build_source', 'build_index', 'build_styles']);
+gulp.task('build', ['build_source', 'build_index', 'build_styles'], function () {
+  // return gulp.src('build/*')
+  //   .pipe(zip('archive.zip'))
+  //   .pipe(size())
+  //   // .pipe(micro({ limit: 13 * 1024 }))
+  //   .pipe(gulp.dest('dist'));
+});
 
-gulp.task('build_source', function() {
-  var bundler = browserify('./src/main', {debug: !prod});
+gulp.task('build_source', function () {
+  // var bundler = browserify('./src/scripts/main', { debug: !prod });
+  var bundler = browserify({
+    entries: [
+      './src/libs/kontra.min',
+      './src/scripts/main',
+      './src/scripts/main-character',
+      './src/scripts/render-queue',
+      './src/scripts/tileset',
+
+    ],
+    debug: !prod
+  });
   if (prod) {
     bundler.plugin(require('bundle-collapser/plugin'));
   }
@@ -51,11 +69,14 @@ gulp.task('build_source', function() {
     .on('error', browserifyError)
     .pipe(source('build.js'))
     .pipe(buffer())
-    .pipe(gulpif(prod, uglify()))
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(ignore.exclude(["**/*.map"]))
+    // .pipe(gulpif(prod, uglify()))
     .pipe(gulp.dest('build'));
+
 });
 
-gulp.task('build_index', function() {
+gulp.task('build_index', function () {
   return gulp.src('src/index.html')
     .pipe(gulpif(prod, htmlmin({
       collapseWhitespace: true,
@@ -65,7 +86,7 @@ gulp.task('build_index', function() {
     .pipe(gulp.dest('build'));
 });
 
-gulp.task('build_styles', function() {
+gulp.task('build_styles', function () {
   return gulp.src('src/styles.less')
     .pipe(less())
     .pipe(concat('build.css'))
@@ -73,18 +94,18 @@ gulp.task('build_styles', function() {
     .pipe(gulp.dest('build'));
 });
 
-gulp.task('clean', function() {
+gulp.task('clean', function () {
   rimraf.sync('build');
   rimraf.sync('dist');
 });
 
-gulp.task('lint', function() {
+gulp.task('lint', function () {
   return gulp.src(['*.js', 'src/**/*.js'])
     .pipe(eslint())
     .pipe(eslint.format());
 });
 
-gulp.task('dist', ['build'], function() {
+gulp.task('dist', ['build'], function () {
   if (!prod) {
     gutil.log(gutil.colors.yellow('WARNING'), gutil.colors.gray('Missing flag --prod'));
     gutil.log(gutil.colors.yellow('WARNING'), gutil.colors.gray('You should generate production assets to lower the archive size'));
@@ -93,22 +114,22 @@ gulp.task('dist', ['build'], function() {
   return gulp.src('build/*')
     .pipe(zip('archive.zip'))
     .pipe(size())
-    .pipe(micro({limit: 13 * 1024}))
+    .pipe(micro({ limit: 13 * 1024 }))
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
   gulp.watch('src/**/*.js', ['lint', 'build_source']);
   gulp.watch('src/styles.less', ['build_styles']);
   gulp.watch('src/index.html', ['build_index']);
 });
 
-gulp.task('serve', ['build'], function() {
+gulp.task('serve', ['build'], function () {
   var htdocs = path.resolve(__dirname, 'build');
   var app = express();
 
   app.use(express.static(htdocs));
-  app.listen(3000, function() {
+  app.listen(3000, function () {
     gutil.log("Server started on '" + gutil.colors.green('http://localhost:3000') + "'");
   });
 });
